f369f148a0db3406cc6e513f56a90966b33817c3
Muhammad Nauman Ghani <ghani_it@hotmail.com>
2022-07-05 11:30:33 +1000

    enum controller and query

---
+++ Application/Common/Enums/EnumController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreIMS.Application.EnumQueries;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CoreIMS.API.Controllers.ItemDefinition
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Authorize]
+
+    public class EnumController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger _logger;
+        public EnumController(ILogger<EnumController> logger, IMediator mediator)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+      
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            return Ok(await _mediator.Send(new LookupEnumsQuery()));
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEnum(int id)
+        {
+            return Ok();
+            //return Ok(await _mediator.Send(new GetEnumQueryByIdQuery() { Id = id }));
+        }
+
+    }
+}
---
+++ Application/Common/Enums/EnumsQuery.cs
@@ -0,0 +1,39 @@
+﻿using CoreIMS.Application.Common.Models;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreIMS.Application.EnumQueries
+{
+    /// <summary>
+    /// Represent the query to lookup all enums
+    /// </summary>
+    public class LookupEnumsQuery : IRequest<Dictionary<EnumModelName, IEnumerable<EnumModel>>> { }
+
+
+    /// <summary>
+    /// The Lookup Enum Handler
+    /// </summary>
+    public class LookupEnumsQueryHandler : IRequestHandler<LookupEnumsQuery, Dictionary<EnumModelName, IEnumerable<EnumModel>>>
+    {
+        private readonly ILogger _logger;
+
+        public LookupEnumsQueryHandler(ILogger<LookupEnumsQueryHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<Dictionary<EnumModelName, IEnumerable<EnumModel>>> Handle(LookupEnumsQuery request, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Getting All Enums");
+            Dictionary<EnumModelName, IEnumerable<EnumModel>> res = EnumModel.CreateEnumDictionary();
+
+            return res;
+        }
+    }
+}
